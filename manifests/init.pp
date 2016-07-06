40c17ef2ae6a1b06b7aaca7bf5bdcc0f41d9af20
Jacob Castello <jacob@arin.net>
2016-07-06 12:15:36 -0400

    forcing manual addition of shebangs and moving repos

--- manifests/init.pp
+++ manifests/init.pp
@@ -34,51 +34,15 @@ class scl (
   String $repo_url_rh = $scl::params::repo_url_rh,
   String $repo_gpg_key = $scl::params::repo_gpg_key,
   String $os_maj_release = $scl::params::os_maj_release,
-  $rubies = $scl::params::rubies,
-  $pythons = $scl::params::pythons,
+  $packages = $scl::params::packages,
   Boolean $include_ruby_gem = true,
   Boolean $include_ruby_devel = true,
-  String $gem_source = $scl::params::gem_source
+  String $gem_source = $scl::params::gem_source,
+  $shebangs = $scl::params::shebangs
 ) inherits scl::params {
 
   if $manage_repos == true {
-    yumrepo { 'CentOS-SCLo-scl':
-      ensure   => $repo_present_sclo,
-      descr    => 'CentOS Software Collections',
-      baseurl  => $repo_url_sclo,
-      enabled  => $repo_enabled_sclo,
-      gpgcheck => true,
-      gpgkey   => $repo_gpg_key,
-    }
-
-    yumrepo { 'CentOS-SCLo-scl-rh':
-      ensure   => $repo_present_rh,
-      descr    => "CentOS-${os_maj_release} - SCLo rh",
-      baseurl  => $repo_url_rh,
-      enabled  => $repo_enabled_rh,
-      gpgcheck => true,
-      gpgkey   => $repo_gpg_key,
-    }
-
-    # Storing the scl gpg key in this module
-    file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-SCLo':
-      ensure => present,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0644',
-      source => 'puppet:///modules/scl/RPM-GPG-KEY-CentOS-SIG-SCLo',
-      notify => Exec['import-scl-gpg-key'],
-    }
-    # Manage the gpg key via the file
-    # in this module
-    exec { 'import-scl-gpg-key':
-      path      => '/bin:/usr/bin',
-      command   => "rpm --import ${repo_gpg_key}",
-      unless    => "rpm -q gpg-pubkey-$(echo $(gpg --throw-keyids --keyid-format short < ${repo_gpg_key}) | cut --characters=11-19 | tr '[A-Z]' '[a-z]')",
-      logoutput => 'on_failure',
-      require   => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-SCLo'],
-      before    => Yumrepo['CentOS-SCLo-scl', 'CentOS-SCLo-scl-rh'],
-    }
+    include scl::repos
   }
 
   # This shebang line is
@@ -94,21 +58,18 @@ class scl (
     content => template('scl/scl-shebang.erb'),
   }
 
-  
-  # Probably need to scope the flag for this
-  # in some way since python is in the works
-  if $rubies != undef {
-    validate_array($rubies)
+  if $shebangs != undef {
 
-    scl::ruby { $rubies:
-      require => Yumrepo['CentOS-SCLo-scl-rh', 'CentOS-SCLo-scl'],
-    }
+    validate_array($shebangs)
+
+    scl::shebang { $shebangs: }
   }
 
-  if $pythons != undef {
-    validate_array($pythons)
+  if $packages != undef {
 
-    scl::python { $pythons:
+    validate_array($packages)
+
+    scl::package { $packages:
       require => Yumrepo['CentOS-SCLo-scl-rh', 'CentOS-SCLo-scl'],
     }
   }
