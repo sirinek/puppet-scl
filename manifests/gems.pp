e1c60580171c61bd16a51cb84519b8bc4465d465
Jacob Castello <jacob@arin.net>
2016-07-06 12:45:48 -0400

    moving this variable to the correct place

--- manifests/gems.pp
+++ manifests/gems.pp
@@ -17,7 +17,7 @@ define scl::gems (
     ensure   => $ensure,
     provider => "${scl_gem_provider}_gem",
     source   => $source,
-    require  => Package["${scl_gem_provider}-rubygems"],
+    require  => Package["${scl_ruby_version}-rubygems"],
   }
 
   ensure_packages($scl_gem_long, $gem_defaults)
