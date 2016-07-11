3479c21327f618ba6a87bc69571ef00fcd1a0537
Jacob Castello <jacob@arin.net>
2016-07-11 14:58:52 -0400

    aligning parameter assignments

--- manifests/gems.pp
+++ manifests/gems.pp
@@ -4,9 +4,9 @@
 # a unique resource name
 define scl::gems (
   String $scl_ruby_version = undef,
-  Hash $scl_gems = hiera("scl::${scl_ruby_version}::gems", {}),
-  String $ensure = 'present',
-  String $source = 'https://rubygems.org'
+  Hash $scl_gems           = hiera("scl::${scl_ruby_version}::gems", {}),
+  String $ensure           = 'present',
+  String $source           = 'https://rubygems.org'
 ) {
 
   $scl_gem_long = suffix( $scl_gems, "-${scl_ruby_version}")
