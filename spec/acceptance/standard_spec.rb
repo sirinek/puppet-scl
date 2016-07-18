d55356bc41b094d06653f7d9f8bfa4a25cf1d716
Jacob Castello <jacob@arin.net>
2016-07-18 12:26:16 -0400

    first passing test

--- spec/acceptance/standard_spec.rb
+++ spec/acceptance/standard_spec.rb
@@ -6,18 +6,13 @@ describe 'scl class' do
     it 'should work with no errors based on the example' do
       pp = <<-EOS
       class { 'scl':
-        packages => [ 'rh-ruby22' ],
-        shebangs => [ 'rh-ruby22' ],
+        packages => ['rh-ruby22'],
       }
       EOS
 
       # Run it twice and test for idempotency
       expect(apply_manifest(pp).exit_code).to_not eq(1)
       expect(apply_manifest(pp).exit_code).to eq(0)
-    end
-
-    describe command('scl enable rh-ruby22 --') do
-      its(:exit_status) { is_expected.to eq 0 }
     end
   end
 end
